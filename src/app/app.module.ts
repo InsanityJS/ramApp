e91a3bd77367f49380ffa13fbc04e85daa0c1300
InsanityJs <maks.naymenko@gmail.com>
2021-10-08 14:42:01 +0300

    Second lesson complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,16 @@ import { AppRoutingModule } from './modules/app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './modules/material.module';
+import { CharactersComponent } from './components/characters/characters.component';
+import { LocationsComponent } from './components/locations/locations.component';
+import { EpisodesComponent } from './components/episodes/episodes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharactersComponent,
+    LocationsComponent,
+    EpisodesComponent
   ],
   imports: [
     BrowserModule,
--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
+
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatCardModule } from '@angular/material/card';
+
 const MATERIALMODULES = [
   MatToolbarModule,
   MatButtonModule,
   MatIconModule,
-  MatMenuModule
+  MatMenuModule,
+  MatCardModule
 ];
 
 @NgModule({
